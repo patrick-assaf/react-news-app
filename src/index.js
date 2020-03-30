9b432c103297992e142870bebd7362eb98f56b5b
Patrick Assaf <patrickassaf96@gmail.com>
2020-03-30 11:20:02 -0400

    Add the navigation bar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const HelloWorld = props => (
-    <h1>It Works!</h1>
+const NavigationBar = props => (
+    <>
+    <Navbar bg="dark" variant="dark">
+        <Form inline>
+            <FormControl type="text" placeholder="Enter keyword..." className="search-text-box" />
+        </Form>
+        <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#world">World</Nav.Link>
+        <Nav.Link href="#politics">Politics</Nav.Link>
+        <Nav.Link href="#business">Business</Nav.Link>
+        <Nav.Link href="#technology">Technology</Nav.Link>
+        <Nav.Link href="#sports">Sports</Nav.Link>
+        </Nav>
+    </Navbar>
+    </>
+);
+
+const ArticleCard = props => (
+    <></>
 );
 
 ReactDOM.render(
-    <HelloWorld />, document.getElementById('root')
+    <NavigationBar />, document.getElementById('root')
 );
